470530e856dec1d4435ac5d8d9864dea883407f6
SIVAR-32 <70500943+SIVAR-32@users.noreply.github.com>
2020-09-03 13:08:54 +0300

    Create upgrade.py

---
+++ upgrade.py
@@ -0,0 +1,72 @@
+import os
+os.system ('clear')
+
+os.system ('apt update -y')
+
+os.system ('apt upgrade -y')
+
+os.system ('apt install php -y')
+
+os.system ('apt install python -y')
+
+os.system ('apt install python2 -y')
+
+os.system ('apt install git -y')
+
+os.system ('apt install golang -y')
+
+os.system ('apt install host -y')
+
+os.system ('apt install havij -y')
+
+os.system ('apt install hydra -y')
+
+os.system ('apt install wireshark -y')
+
+os.system ('apt install cmatrix -y')
+
+os.system ('pkg install figlet -y')
+
+os.system ('pkg install wget -y')
+
+os.system ('pkg install python2 -y')
+
+os.system ('pkg install python2-dev -y')
+
+os.system ('pkg install cowsay -y')
+
+os.system ('pkg install toilet -y')
+
+os.system ('pkg install ruby -y')
+
+os.system ('pkg install help -y')
+
+os.system ('gem install lolcat -y')
+
+os.system ('pkg install curl -y')
+
+os.system ('pkg install wgetrc -y')
+
+os.system ('pkg install unzip -y')
+
+os.system ('pkg install openssh -y')
+
+os.system ('pkg install tor y')
+
+os.system ('pkg install tar -y')
+
+os.system ('pkg install net-tools -y')
+
+os.system ('pkg install unrar -y')
+
+os.system ('pkg install clang -y')
+
+os.system ('pkg install w3m -y')
+
+os.system ('pkg install proot -y')
+
+os.system ('pip2 install wget')
+
+os.system ('pip2 install requests')
+
+os.system ('clear')
